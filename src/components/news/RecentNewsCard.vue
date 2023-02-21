8051486cbefb0d0e0b2307ad5a9ef646349d18dd
bexalho <aljesa.hoxha@bexeo.com>
2023-02-21 10:10:40 +0000

    working in the recent news items

--- src/components/news/RecentNewsCard.vue
+++ src/components/news/RecentNewsCard.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="bg-white p-5 border border-gray-200 rounded">
         <main-heading :title="title" tag="h5" />
+        <recent-news-item title="Our Company Wins Award for Innovation in Software Development" imgSrc="src/assets/images/news/news-1.jpeg" url="/" date="21/02/2023" />
     </div>
 </template>
 <script>
 import MainHeading from '../reusable/MainHeading.vue'
+import RecentNewsItem from './RecentNewsItem.vue'
 export default {
-    components: { MainHeading },
+    components: { MainHeading, RecentNewsItem },
     props: {
         title: {
             type: String,
---
+++ src/components/news/RecentNewsItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex">
+        <div>
+            <img :src="imgSrc" :alt="title">
+        </div>
+        <div>
+            <a :href="link">{{ title }}</a>
+            <span>{{ postDate }}</span>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        imgSrc: {
+            type: String,
+        },
+        title: {
+            type: String,
+        },
+        postDate: {
+            type: String,
+        },
+        link: {
+            type: String,
+        }
+    }
+
+}
+</script>
